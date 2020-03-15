5f62d7458267b819ca38966b85ef322844c16b3f
Philip Lee-Chin <phil.leechin@outlook.com>
2020-03-15 17:10:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,7 +4,7 @@ class Song
   attr_accessor :name
   attr_reader :artist
 
-  extend findable::ClassMethods
+  extend Findable::ClassMethods
   extend Memorable::ClassMethods
   include Memorable::InstanceMethods
 
